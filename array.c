3dedf5c74634bfdad25b64d925bb283389e5ba55
Giovannamendoza <89482693+Giovannamendoza@users.noreply.github.com>
2021-10-15 01:24:16 -0500

    Create array.c

---
+++ array.c
@@ -0,0 +1,28 @@
+include <stdio.h>
+#define N 10 
+
+int array[N]={1,2,5,71,99,0,5,16,26,3};
+
+void Shell(int *array, int n) {
+  int i,x,y,tmp;
+
+  for(i = 1; i < n; i = i*3+1) {} 
+
+    while (i > 0) {
+      for(x = i; x < n; x++) {
+        y = x;
+        tmp = array[x];
+        while (y >= i && array[y - i] > tmp) {
+          array[y] = array[y - i];
+          y = y - i;
+        }
+        array[y] = tmp;
+      }
+      i = i / 2;
+    }
+}
+int main() {
+
+  Shell(vector,allahuakbar);
+  return 0;
+}
